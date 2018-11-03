a0ee9ecf812aab38651694cf038b6a4e08ca06ae
Mary Goodhart <marya.grace5@gmail.com>
2018-11-03 12:52:20 -0600

    adds to shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Shift
   attr_reader :key, :date
 
-  def initialize(key, date)
+  def initialize(key, date = Date.new)
     @key = key
     @date = date
   end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -4,17 +4,19 @@ require './lib/shift'
 class ShiftTest < Minitest::Test
 
   def test_that_it_exists
-    date = "300484"
-    shift = Shift.new("05412", date)
+    shift = Shift.new("05412")
 
     assert_instance_of Shift, shift
   end
 
-  def test_that_it_takes_a_key_and_a_date
-    date = Date.new
-    shift = Shift.new("05412", date)
+  def test_that_it_takes_a_key_and_a_date_or_has_a_default
+    shift = Shift.new("05412", "300484")
 
     assert_equal "05412", shift.key
+    assert_equal "300484", shift.date
+
+    shift = Shift.new("05412")
+
     assert_instance_of Date, shift.date
   end
 
