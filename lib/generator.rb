9c55bab1836d0783553d54cde6f6ad40c119f907
Mary Goodhart <marya.grace5@gmail.com>
2018-11-04 16:05:02 -0700

    generator tests

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,4 +1,11 @@
 require 'pry'
 
 class Generator
+  attr_reader :numbers
+
+  def initialize
+    @numbers = [*0..9]
+  end
+
+
 end
--- test/generator_test.rb
+++ test/generator_test.rb
@@ -14,4 +14,10 @@ class GeneratorTest < Minitest::Test
 
     assert_equal [0,1,2,3,4,5,6,7,8,9], generator.numbers
   end
+
+  def test_that_it_can_produce_randomly_generated_numbers
+    generator = Generator.new
+
+    assert_equal [34512], generator.randomizer
+  end
 end
