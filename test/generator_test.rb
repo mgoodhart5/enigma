5cf24dec20132d5f73a10fff0f9ffa4d4ebaba7c
Mary Goodhart <marya.grace5@gmail.com>
2018-11-04 16:01:11 -0700

    testing for generator method

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -2,9 +2,16 @@ require './test/test_helper'
 require './lib/generator'
 
 class GeneratorTest < Minitest::Test
+
   def test_that_it_exists
     generator = Generator.new
-    
-    assert_instance_of generator, Generator
+
+    assert_instance_of Generator, generator
+  end
+
+  def test_that_it_is_initialized_with_numbers
+    generator = Generator.new
+
+    assert_equal [0,1,2,3,4,5,6,7,8,9], generator.numbers
   end
 end
