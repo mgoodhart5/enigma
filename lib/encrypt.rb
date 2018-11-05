ff50844eb9040bb84abdd325df6ad81dd64aa197
Mary Goodhart <marya.grace5@gmail.com>
2018-11-05 12:48:43 -0700

    working on command line interface

---
+++ lib/encrypt.rb
@@ -0,0 +1,14 @@
+require
+message, encrypted_file = ARGV
+enigma = Enigma.new
+#open the message to be readable
+open encrypt file to be writeable
+encrypt method
+write in the encrypted.write file.
+
+
+
+
+
+puts "Created '#{encrypted_file}' with the key "
+#interpolation for these things
